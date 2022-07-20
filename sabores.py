c1efc812e51e761eb958b4693a925e85306f3466
Emely4 <109684712+Emely4@users.noreply.github.com>
2022-07-20 10:22:59 -0600

    Add files via upload

---
+++ sabores.py
@@ -0,0 +1,16 @@
+sabores=input("Digite un sabor de sorbete") 
+if sabores =="chocolate": 
+#print("El precio es de $0.50") 
+precio="$0.50" 
+elif sabores== "vainilla": 
+#print ("El precio es de $1.00") 
+precio="$1.00" 
+elif sabores=="menta": 
+#print ("El precio es de $0.75") 
+precio="$0.75" 
+elif sabores=="chicle": 
+#print ("el precio es de $2.50") 
+precio="$2.50" 
+else: 
+print ("Vuelva pronto hoy no hay sabor del sabor que has elegido") 
+print("El sabor que elegiste es:"+sabores+"El precio es"+precio) 
